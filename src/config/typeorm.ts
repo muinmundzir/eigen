9ce85bb63eea48e88ac3b54fa87419226f093304
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-05-19 12:00:35 +0700

    refactor: update config file

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -11,8 +11,8 @@ const config = {
   username: `${process.env.DB_USERNAME}`,
   password: `${process.env.DB_PASSWORD}`,
   database: `${process.env.DB_NAME}`,
-  entities: ['./**/*.entity{.ts,.js}'],
-  migrations: ['./migrations/*{.ts,.js}'],
+  entities: ['dist/src/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
   synchronize: false,
 };
