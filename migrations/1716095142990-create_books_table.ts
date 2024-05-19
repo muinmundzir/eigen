8f18a2d9d442bd7b8b449d02288fe5c57e98b817
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-05-19 12:08:10 +0700

    feat: add create books table migration

---
+++ migrations/1716095142990-create_books_table.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateBooksTable1716095142990 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'books',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            default: 'uuid_generate_v4()',
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'code',
+            type: 'varchar',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'author',
+            type: 'varchar',
+          },
+          {
+            name: 'stock',
+            type: 'numeric',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('books');
+  }
+}
